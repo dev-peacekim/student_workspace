9c0ce2cbf4112271930d9b3a86a28a8c75d31716
LeeHanSu <dlgkstnrn@naver.com>
2024-03-08 15:41:28 +0900

    03 08 admin 메인페이지 그래프 완료

--- s20240130103/src/main/resources/static/assets/js/lhs/lhsadmin.js
+++ s20240130103/src/main/resources/static/assets/js/lhs/lhsadmin.js
@@ -1,42 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
 	$.ajax({
 		type: 'get',
+		dataType:'json',
 		url : 'userCntListAjax',
 		success:function(result){
-			console.log(result);
 			new Chart(document.querySelector("#barChart"), {
                       type: "bar",
                       data: {
                         labels: [
-                          "January",
-                          "February",
-                          "March",
-                          "April",
-                          "May",
-                          "June",
-                          "July",
+                          result['4']['DAY'],
+                          result['3']['DAY'],
+                          result['2']['DAY'],
+                          result['1']['DAY'],
+                          result['0']['DAY'],
                         ],
                         datasets: [
                           {
                             label: "Bar Chart",
-                            data: [65, 59, 80, 81, 56, 55, 40],
+                            data: [
+								result['4']['USER_CNT'], 
+								result['3']['USER_CNT'], 
+								result['2']['USER_CNT'], 
+								result['1']['USER_CNT'], 
+								result['0']['USER_CNT']
+							],
                             backgroundColor: [
                               "rgba(255, 99, 132, 0.2)",
                               "rgba(255, 159, 64, 0.2)",
                               "rgba(255, 205, 86, 0.2)",
                               "rgba(75, 192, 192, 0.2)",
-                              "rgba(54, 162, 235, 0.2)",
-                              "rgba(153, 102, 255, 0.2)",
-                              "rgba(201, 203, 207, 0.2)",
+                              "rgba(54, 162, 235, 0.2)"
                             ],
                             borderColor: [
                               "rgb(255, 99, 132)",
                               "rgb(255, 159, 64)",
                               "rgb(255, 205, 86)",
                               "rgb(75, 192, 192)",
-                              "rgb(54, 162, 235)",
-                              "rgb(153, 102, 255)",
-                              "rgb(201, 203, 207)",
+                              "rgb(54, 162, 235)"
                             ],
                             borderWidth: 1,
                           },
