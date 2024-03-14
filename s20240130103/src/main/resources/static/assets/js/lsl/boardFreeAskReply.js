5a5067a20c98f918f02c22437b8d9a984c8a853f
saelip <newjj03@naver.com>
2024-03-14 20:50:12 +0900

    개잎 커밋 대댓글 구현중

--- s20240130103/src/main/resources/static/assets/js/lsl/boardFreeAskReply.js
+++ s20240130103/src/main/resources/static/assets/js/lsl/boardFreeAskReply.js
@@ -1,5 +1,3 @@
-let currentuser_no;
-
 // 유저 정보 가져오기 
 function getUserNo() {
 	$.ajax({
@@ -55,26 +53,27 @@ function replyBoardFreeAskList(cboard_no) {
 
 // 게시판 댓글 리스트 갱신 
 function updateReplyList(data) {
-    let boardReplyList = '';
-    $.each(data, function(index, lslCommReply) {
-        console.log(lslCommReply);
-        const originalDate = new Date(lslCommReply.creply_date);
-        let hour = originalDate.getHours();
-        const ampm = hour >= 12 ? '오후' : '오전';
-        hour = hour % 12 || 12; // 0시일 때 12시로 변경
-        const formatDate = `${originalDate.getFullYear()}.${(originalDate.getMonth() + 1).toString().padStart(2, '0')}.${originalDate.getDate().toString().padStart(2, '0')} ${ampm} ${hour.toString().padStart(2, '0')}:${originalDate.getMinutes().toString().padStart(2, '0')}`;
-        let ReplyUser = lslCommReply.user_no;
+	let boardReplyList = '';
+	$.each(data, function(index, lslCommReply) {
 
-        console.log(currentuser_no);
-        console.log(ReplyUser);
+		console.log(lslCommReply);
 
-        // 현재 세션 사용자와 댓글 작성자가 동일한 경우에만 수정 및 삭제 버튼 표시
-        const showButtons = currentuser_no === ReplyUser;
+		const originalDate = new Date(lslCommReply.creply_date);
 
-        // 대댓글 입력창의 ID를 부모 댓글 번호에 기반하여 유일하게 만듭니다.
-        const addCommentId = `addComment_${lslCommReply.creply_no}`;
+		let hour = originalDate.getHours();
+		const ampm = hour >= 12 ? '오후' : '오전';
+		hour = hour % 12 || 12; // 0시일 때 12시로 변경
+		const formatDate = `${originalDate.getFullYear()}.${(originalDate.getMonth() + 1).toString().padStart(2, '0')}.${originalDate.getDate().toString().padStart(2, '0')} ${ampm} ${hour.toString().padStart(2, '0')}:${originalDate.getMinutes().toString().padStart(2, '0')}`;
 
-        boardReplyList += `<div class="re-comment-body">
+		let ReplyUser = lslCommReply.user_no;
+
+		console.log(currentuser_no);
+		console.log(ReplyUser);
+
+		// 현재 세션 사용자와 댓글 작성자가 동일한 경우에만 수정 및 삭제 버튼 표시
+		const showButtons = currentuser_no === ReplyUser;
+
+		boardReplyList += `<div class="re-comment-body">
             <div id="replyBoardFreeList" class="comment-card">
                 <div class="comment-header">
                     <img class="comment-user-profile" src="${lslCommReply.user_profile}">
@@ -85,30 +84,31 @@ function updateReplyList(data) {
                     <div class="re-btn-container">
                         <button type="button" class="btn brModify" onclick="toggleEdit('${lslCommReply.creply_no}');" style="display: ${showButtons ? 'block' : 'none'};">수정</button>
                         <button type="button" class="btn brDelete" onclick="deleteComment('${lslCommReply.cboard_no}', '${lslCommReply.creply_no}');" style="display: ${showButtons ? 'block' : 'none'};">삭제</button>
-                        <button type="button" class="btn brBtn" data-parent-creply="${lslCommReply.creply_no}", data-creply-no="${lslCommReply.creply_no}", data-creply-level="${lslCommReply.creply_level}" onclick="toggleReply('${lslCommReply.creply_no}');"><i class="bi bi-reply-fill"></i></button>
+            		    <button type="button" class="btn brBtn" data-parent-creply="${lslCommReply.creply_no}", data-creply-no="${lslCommReply.creply_no}", data-creply-level="${lslCommReply.creply_level}" onclick="toggleReply('${lslCommReply.creply_no}');"><i class="bi bi-reply-fill"></i></button>
                     </div>
                 </div>
                 <div id="replyBox">
-                    <div id="editComment_${lslCommReply.creply_no}" class="card-body comment-body" style="display: none;">
-                        <textarea id="editCommentText_${lslCommReply.creply_no}" class="form-control">${lslCommReply.creply_content}</textarea>
-                        <button type="button" class="btn brSave" onclick="modifyComment('${lslCommReply.creply_no}');">저장</button>
-                        <button type="button" class="btn brCancle" onclick="toggleEdit('${lslCommReply.creply_no}');">취소</button>
-                    </div>
+                <div id="editComment_${lslCommReply.creply_no}" class="card-body comment-body" style="display: none;">
+                    <textarea id="editCommentText_${lslCommReply.creply_no}" class="form-control">${lslCommReply.creply_content}</textarea>
+                    <button type="button" class="btn brSave" onclick="modifyComment('${lslCommReply.creply_no}');">저장</button>
+               		<button type="button" class="btn brCancle" onclick="toggleEdit('${lslCommReply.creply_no}');">취소</button>
                 </div>
-                <div id="viewComment_${lslCommReply.creply_no}" class="card-body comment-body">
+                </div>
+                
+                 <div id="viewComment_${lslCommReply.creply_no}" class="card-body comment-body">
                     <p class="markdown-body" id="creply_content${lslCommReply.creply_no}">${lslCommReply.creply_content}</p>
                 </div>
-                <!-- 대댓글 입력창 -->
-                <div id="${addCommentId}" class="card-body comment-body" style="display: none;">
-                    <textarea id="addCommentText_${lslCommReply.creply_no}" class="form-control"></textarea>
-                    <button type="button" class="btn addReply" onclick="addReply('${lslCommReply.creply_no}');">저장</button>
-                    <button type="button" class="btn replyCancle" onclick="toggleReply('${lslCommReply.creply_no}');">취소</button>
-                </div>
+                
+                <div id="addComment_${lslCommReply.creply_no}" class="card-body comment-body" style="display: none;">
+		          <textarea id="addCommentText" class="form-control"></textarea>
+		          <button type="button" class="btn addReply" id="addReply"onclick="addReply('${lslCommReply.creply_no}');">저장</button>
+		         <button type="button" class="btn replyCancle" onclick="toggleReply('${lslCommReply.creply_no}');">취소</button>
+		  </div>
             </div>
         </div>`;
-    });
+	});
 
-    $('#boardReplyForm').html(boardReplyList);
+	$('#boardReplyForm').html(boardReplyList);
 }
 
 
@@ -161,9 +161,6 @@ function addComment(replyData) {
 
 
 
-
-
-/* 대댓글 */
 let creply_no;
 
 
@@ -198,6 +195,21 @@ $(document).on('click', ".addReply", function() {
 // 만약 이런식으로 넘길 경우 controller에서 @RequestBody 빼고 받아야 한다.
 // 단 JSON일 경우 안됨.
 
+/*data: reReplyData를 사용할 경우:
+
+이 경우에는 reReplyData 객체가 그대로 전달됩니다. 즉, 객체의 속성과 값을 그대로 전송합니다.
+Controller에서는 @RequestBody 어노테이션을 사용하여 이 데이터를 수신할 수 있습니다.
+단, 이 경우에는 JSON 형식이 아니기 때문에 @RequestBody로는 JSON 형식의 데이터를 수신할 수 없다.
+
+data: JSON.stringify(reReplyData)를 사용할 경우:
+
+이 경우에는 reReplyData 객체를 JSON 문자열로 변환하여 전송합니다.
+Controller에서는 JSON 형식의 데이터를 수신하기 위해 @RequestBody 어노테이션을 사용할 수 있습니다.
+즉, 이 방법은 JSON 형식의 데이터를 전송하고 수신할 때 사용
+
+
+*/
+
 
 // 대댓글 등록
 function addReply(reReplyData) {
@@ -212,7 +224,7 @@ function addReply(reReplyData) {
             if (data > 0) {
                 console.log('대댓글 데이터 넘어옴');
                 // 대댓글 등록 후 댓글 리스트 갱신 시 cboard_no 전달
-                replyBoardFreeAskList(reReplyData.cboard_no); // 수정: reReplyData.cboard_no로 변경
+                replyBoardFreeAskList(reReplyData.cboard_no);
             }
         },
         error: function(error) {
@@ -221,11 +233,6 @@ function addReply(reReplyData) {
         }
     });
 }
-
-
-
-
-
 
 
 // 수정 창을 토글하는 함수
