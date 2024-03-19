af5ec5be46e952e77146c5bc72bd456166a547dd
dlgkstnrn <51310682+dlgkstnrn@users.noreply.github.com>
2024-03-19 10:02:51 +0900

    03 19 이한수 주소록 디자인 수정

--- s20240130103/src/main/resources/static/assets/js/lhs/lhsaddressaddForm.js
+++ s20240130103/src/main/resources/static/assets/js/lhs/lhsaddressaddForm.js
@@ -7,20 +7,26 @@ document.getElementById("searchbtn").addEventListener("click",function(){
 		data: {'user_id': inputresult},
 		dataType : 'json',
 		success : function(result){
-			console.log(result);
+			let profileName;
+			let score = Math.floor(result.eval_score);
+			if(result.user_profile === null){
+					profileName = "987654321487321564defaultImg.jpg";
+			}else{
+					profileName = result.user_profile
+			}
 			if(result.address_chk == '1'){
 				$(".address-main").append(`
 				<div class="address-main-inner">
 		            <div class="address-list-card-detail">
 		              <div class="profile-img-user-name">
-		                <img src="upload/userImg/${result.user_profile}" alt="Profile" class="rounded-circle address-list-profile-img">
+		                <img src="upload/userImg/${profileName}" alt="Profile" class="rounded-circle address-list-profile-img">
 		                <div>
 		                  <p class="user-name">${result.user_name}</p>
 		                  <p class="user-id">#${result.user_id}</p>
 		                </div>
 		              </div>
 		              <div class="score-message">
-		              	<div class="user-score rounded-circle justify-content-center">${result.eval_score}</div> 
+		              	<div class="user-score rounded-circle justify-content-center">${score}</div> 
 		              </div>
 		            </div>
 		          </div>
@@ -37,7 +43,7 @@ document.getElementById("searchbtn").addEventListener("click",function(){
 		                </div>
 		              </div>
 		              <div class="score-message">
-		              	<div class="user-score rounded-circle justify-content-center">${result.eval_score}</div> 
+		              	<div class="user-score rounded-circle justify-content-center">${score}</div> 
 		              	<div>
 			              	<a href="/addressAdd?re_user_no=${result.user_no}" style="color:white;">
 				                <div class="user-score rounded-circle justify-content-center" >
