a0e15525e664bf03a14619512248daf844b7a3d7
dlgkstnrn <51310682+dlgkstnrn@users.noreply.github.com>
2024-03-20 10:35:04 +0900

    03 20 이한수 드래그앤 드롭 수정중

--- s20240130103/src/main/resources/static/assets/js/lsl/boardFile.js
+++ s20240130103/src/main/resources/static/assets/js/lsl/boardFile.js
@@ -1,26 +1,50 @@
+const fileArray = []; //파일 저장하는 배열
+const dataTransfer = new DataTransfer();
+const dataList = dataTransfer.items;
+var fileListDiv = document.getElementById('fileList');
+var formFileInput = document.getElementById('formFile');
 $(document).ready(function() {
-    var formFileInput = document.getElementById('formFile');
-    var fileListDiv = document.getElementById('fileList');
-
     formFileInput.addEventListener('change', function() {
         fileListDiv.innerHTML = '';
-
         var files = this.files;
         if (files.length === 0) {
             fileListDiv.textContent = '파일이 없습니다.';
             return;
         }
-
         for (var i = 0; i < files.length; i++) {
             var file = files[i];
+            dataList.add(file);
             var fileName = file.name;
-
             var listItem = document.createElement('div');
             listItem.textContent = fileName;
             fileListDiv.appendChild(listItem);
-
-            // 파일 이름 다음에 줄 바꿈 추가
-            fileListDiv.appendChild(document.createElement('br'));
         }
+        console.log(dataList);``
     });
 });
+
+// -----------------드래그 앤 드롭 테스트중-------------------------
+
+document.querySelector('.upload-title').addEventListener('dragover',function(e){
+	e.preventDefault();
+	e.stopPropagation();
+});
+document.querySelector('.upload-title').addEventListener('drop',function(e){
+	e.preventDefault();
+	e.stopPropagation();
+    var file = e.dataTransfer?.files[0];
+    var fileName = file.name;
+    var listItem = document.createElement('div');
+    listItem.textContent = fileName;
+    fileListDiv.appendChild(listItem);
+    dataList.add(file);
+    console.log(dataList);
+})
+
+function filesChk(){
+	formFileInput.files = dataTransfer.files;
+	console.log(formFileInput.files);
+	return true;
+}
+
+
